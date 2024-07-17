26c924e857c3c587cfd0af2943723c2d1e11d4b9
Manu J <61179639+manuj55@users.noreply.github.com>
2024-07-17 23:11:04 +0200

    Trekking heaven(trekking tools): First draft
    * Inital setup
    
    * adding test files
    
    * Landing page (#3)
    
    * initial
    
    * path changes
    
    * packing items card list added
    
    * gardient
    
    * added best picks
    
    * bug fixes and material design
    
    * responsive
    
    * added custom fonts and logo
    
    * Store page  (#4)
    
    * initial layout design
    
    * updated card style
    
    * updated sort by logic
    
    * updated styling
    
    * updated color for about page
    
    * About page (#5)
    
    * aboutcommit
    
    * added responsive css and small changes
    
    * All pages reference link and integration (#6)
    
    * initial layout design
    
    * updated card style
    
    * updated sort by logic
    
    * updated styling
    
    * updated color for about page
    
    * updated about us page style
    
    * Landing page (#7)
    
    * initial
    
    * path changes
    
    * packing items card list added
    
    * gardient
    
    * added best picks
    
    * bug fixes and material design
    
    * responsive
    
    * added custom fonts and logo
    
    * navbar responsive
    
    * custom font
    
    * update button color
    
    * Landing page (#8)
    
    * initial
    
    * path changes
    
    * packing items card list added
    
    * gardient
    
    * added best picks
    
    * bug fixes and material design
    
    * responsive
    
    * added custom fonts and logo
    
    * navbar responsive
    
    * custom font
    
    * tail wind conflits removed
    
    * navigation font weight and color
    
    ---------
    
    Co-authored-by: sg0307821 <126344283+sg0307821@users.noreply.github.com>
    Co-authored-by: Pramukh Prakash <pramukhp35@gmail.com>

---
+++ js/about.js
@@ -0,0 +1,126 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const crviewmorebtn = document.getElementById("cr-viewmore");
+    const crmoredetails = document.getElementById("cr-moredetails");
+    const crviewlessbtn = document.getElementById("cr-viewless");
+
+    const dismoredetails = document.getElementById("disclaimer-moredetails");
+    const disviewmorebtn = document.getElementById("disclaimer-viewmore");
+    const disviewlessbtn = document.getElementById("disclaimer-viewless");
+
+    const conmoredetails = document.getElementById("conatct-moredetails");
+    const conviewmorebtn = document.getElementById("contact-viewmore");
+    const conviewlessbtn = document.getElementById("contact-viewless");
+
+    const termmoredetails = document.getElementById("terms-moredetails");
+    const termviewmorebtn = document.getElementById("terms-viewmore");
+    const termviewlessbtn = document.getElementById("terms-viewless");
+
+
+    const feedbackform = document.getElementById("feedback-form");
+    const fullname = document.getElementById("fullname");
+    const phonenumber = document.getElementById("phonenumber");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+    const nameerror = document.getElementById("name-error");
+    const phoneerror = document.getElementById("phone-error");
+    const emailerror = document.getElementById("email-error");
+    const texterror = document.getElementById("text-error");
+    const namepattern = /^[A-Za-z\s]+$/;
+    const phonepattern = /^[0-9]+$/;
+    const emailpattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const successmessage = document.getElementById("successMessage");
+
+
+    crviewmorebtn.addEventListener("click", () => {
+        crmoredetails.classList.remove("hidden");
+        crviewmorebtn.classList.add("hidden");
+    });
+
+    crviewlessbtn.addEventListener("click", () => {
+        crmoredetails.classList.add("hidden");
+        crviewmorebtn.classList.remove("hidden");
+
+    });
+
+    disviewmorebtn.addEventListener("click", () => {
+        dismoredetails.classList.remove("hidden");
+        disviewmorebtn.classList.add("hidden");
+
+    });
+
+    disviewlessbtn.addEventListener("click", () => {
+        dismoredetails.classList.add("hidden");
+        disviewmorebtn.classList.remove("hidden");
+    });
+
+    conviewmorebtn.addEventListener("click", () => {
+        conmoredetails.classList.remove("hidden");
+        conviewmorebtn.classList.add("hidden");
+    })
+
+    conviewlessbtn.addEventListener("click", () => {
+        conmoredetails.classList.add("hidden");
+        conviewmorebtn.classList.remove("hidden");
+
+    })
+
+
+    termviewmorebtn.addEventListener("click", () => {
+        termmoredetails.classList.remove("hidden");
+        termviewmorebtn.classList.add("hidden");
+    })
+
+    termviewlessbtn.addEventListener("click", () => {
+        termmoredetails.classList.add("hidden");
+        termviewmorebtn.classList.remove("hidden");
+
+    })
+
+    feedbackform.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let result = true;
+        nameerror.textContent = "";
+        phoneerror.textContent = "";
+        emailerror.textContent = "";
+        texterror.textContent = "";
+
+        if (fullname.value.trim() === "" || !namepattern.test(fullname.value)) {
+            nameerror.textContent = "Enter a valid name";
+            result = false;
+        }
+
+        if (!phonepattern.test(phonenumber.value)) {
+            phoneerror.textContent = "Enter a valid number";
+            result = false;
+        }
+
+        if (!emailpattern.test(email.value)) {
+            emailerror.textContent = "Enter a valid email";
+            result = false;
+        }
+
+        if (message.value.trim() === "") {
+            texterror.textContent = "Enter a valid message";
+            result = false;
+        }
+
+        if (result) {
+            successmessage.style.display = "block";
+            const info =
+            {
+                FullName: fullname.value,
+                PhoneNumber: phonenumber.value,
+                Email: email.value,
+                Feedaback: message.value,
+            };
+            window.alert(`
+                Full name: ${info.FullName}
+                PhoneNumber: ${info.PhoneNumber}
+                Email: ${info.Email},
+                Feedaback: ${info.Feedaback}`)
+            console.log(info);
+        }
+    })
+
+});
---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger-button");
+    const navLinks = document.querySelector(".menu");
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+});
+
+
